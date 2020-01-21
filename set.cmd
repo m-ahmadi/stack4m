630872f8a6f00b8c37e47ead2a5d62a51b40c006
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-01-21 04:46:01 +0330

    minor variable rename in set.cmd

--- set.cmd
+++ set.cmd
@@ -1,6 +1,6 @@
 @echo off
 PATH=%PATH%;./node_modules/.bin
-for /f "tokens=*" %%g IN ('dir #* /b') do (set CWP=%%g)
+for /f "tokens=*" %%v IN ('dir #* /b') do (set CWP=%%v)
 set CWP=%CWP:#=%
 set SRC=.
 set DEST=./public
